b6987b55ad0cc92e2b2219680d2792478ec0f12c
201344085_LyuSeongMin <j1344085@naver.com>
2016-11-25 18:32:24 +0900

    메뉴 생성을 위한 액티비티 추가 및 연결소스추가

--- app/src/main/java/com/example/ryun/calc/Main3Activity.java
+++ app/src/main/java/com/example/ryun/calc/Main3Activity.java
@@ -2,12 +2,23 @@ package com.example.ryun.calc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
+
+    Button btn1;
+    Button btn2;
+    Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        btn1 = (Button)findViewById(R.id.button7);
+        btn2 = (Button)findViewById(R.id.button8);
+        btn3 = (Button)findViewById(R.id.button9);
+
+
     }
 }
