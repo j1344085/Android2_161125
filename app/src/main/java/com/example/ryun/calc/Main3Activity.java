72d121697df02c7db8134544c71b1b72cdffc918
201344085_LyuSeongMin <j1344085@naver.com>
2016-11-25 18:37:51 +0900

    메뉴 생성을 위한 액티비티 추가 및 연결소스추가

--- app/src/main/java/com/example/ryun/calc/Main3Activity.java
+++ app/src/main/java/com/example/ryun/calc/Main3Activity.java
@@ -1,7 +1,10 @@
 package com.example.ryun.calc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
@@ -19,6 +22,31 @@ public class Main3Activity extends AppCompatActivity {
         btn2 = (Button)findViewById(R.id.button8);
         btn3 = (Button)findViewById(R.id.button9);
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("onClick", "MainActivity");
+                Intent intentSubActivity = new Intent(Main3Activity.this, MainActivity.class);
+                startActivity(intentSubActivity);
+            }
+        });
 
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("onClick", "Main4Activity");
+                Intent intentSubActivity = new Intent(Main3Activity.this, Main4Activity.class);
+                startActivity(intentSubActivity);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("onClick", "Main5Activity");
+                Intent intentSubActivity = new Intent(Main3Activity.this, Main5Activity.class);
+                startActivity(intentSubActivity);
+            }
+        });
     }
 }
